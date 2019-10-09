83bda30acbd067ba3ed9864932b9e04b09483e9b
Nory Lazo <nlazo@icloud.com>
2019-10-09 06:22:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(numbers)
   index = 0 
   
   while index < numbers.length do
-    new_numbers[index] = numbers.index ** 2
+    new_numbers[index] = numbers[index] ** 2
   end 
 
 end
