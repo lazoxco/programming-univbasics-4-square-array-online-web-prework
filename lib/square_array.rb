89127251ac608e1349a7014c40cbe82f5f5ca1d0
Nory Lazo <nlazo@icloud.com>
2019-10-09 06:31:15 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,16 @@
-def square_array(array)
-  # your code here
+# Define a method that takes an array on numbers and returns a new array of squared numbers
+
+def square_array(numbers)
+  
+    result = [] # New array that will be returned
+    
+    index = 0 
+    
+    while index < numbers.length do
+      result[index] = numbers[index]**2 
+      index += 1 
+    end 
+    
+  result
+  
 end
